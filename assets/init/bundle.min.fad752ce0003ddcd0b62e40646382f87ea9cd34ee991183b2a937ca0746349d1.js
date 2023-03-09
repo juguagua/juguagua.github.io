06324e5ad85dd567972400b30b9f7fd268861f8f
juguagua <juguaguaji@gmail.com>
2023-03-10 00:45:38 +0800

    更换主题

---
+++ assets/init/bundle.min.fad752ce0003ddcd0b62e40646382f87ea9cd34ee991183b2a937ca0746349d1.js
@@ -0,0 +1,164 @@
+(() => {
+  // ns-params:@params
+  var params_default = { analytics: null, archive: { paginate: 20 }, baseURL: "/", brand: "HBS", breadcrumb: true, codeblock: { linenos: false }, color: "auto", contact: { endpoint: "https://getform.io/f/56041c81-9e03-4c24-b9c5-61238854d4cd" }, counttaxonomyposts: true, creativecommons: null, customcss: null, customjs: null, dateformat: ":date_long", description: "An extreme fast, responsive and feature-rich blog theme for Hugo", diagram: false, featuredpostcount: 5, fixedheader: true, fontsize: null, fullwidth: false, googleadsense: null, keywords: "Hugo, Bootstrap, Blog Theme", languagename: "English", mainsections: ["blog", "posts", "news"], palette: "purple", palettes: ["blue", "blue gray", "brown", "cyan", "green", "indigo", "orange", "pink", "purple", "red", "teal", "yellow"], pinnedpost: true, pinnedpostcount: 3, post: { featuredimage: true, numberifyheadings: true, numberifyheadingsseparator: "." }, postdate: true, poweredby: true, pwa: { manifest: { short_name: "HBS" } }, readingtime: true, recentpostcount: 5, relatedpostcount: 5, repo: { branch: "main", url: "https://github.com/razonyang/hugo-theme-bootstrap-skeleton" }, reward: { alipay: "images/reward/alipay.png", wechat: "images/reward/wechat.png" }, search: { fuse: { threshold: 0.1 }, paginate: 5 }, searchbar: true, sidebar: null, sidebartaxonomies: ["series", "categories", "tags", "authors"], siteverification: { baidu: null, baiduunion: null, bing: null, google: null, shenma: null, so: null, sogou: null }, taxonomypaginate: 10, taxonomypostcount: 20, titlecase: true, titleseparator: "-", tocwordcount: 1, topappbar: { social: { github: "razonyang/hugo-theme-bootstrap", patreon: "razonyang", paypal: "razonyang" } }, utterances: { label: "comment", repo: "razonyang/hugo-theme-bootstrap-skeleton" }, viewer: true, weight: 1 };
+
+  // ns-hugo:D:\GitHub\Blog\themes\hugo-theme-bootstrap\assets\js\local-storage\index.ts
+  var PathLocalStorage = class {
+    constructor(baseURL) {
+      this.baseURL = baseURL;
+      this.prefix = "hbs:";
+      if (baseURL.substring(0, 2) === "//") {
+        baseURL = "http:" + baseURL;
+      }
+      let url;
+      try {
+        url = new URL(baseURL);
+      } catch (e) {
+        url = new URL(baseURL, location.protocol + "//" + location.host);
+      }
+      const pathname = url.pathname.replace(/^(\/+)/, "").replace(/(\/+)$/, "");
+      if (pathname !== "") {
+        this.prefix += pathname.replace("/", "-") + ":";
+      }
+    }
+    getItem(key) {
+      return localStorage.getItem(this.prefix + key);
+    }
+    setItem(key, value) {
+      localStorage.setItem(this.prefix + key, value);
+    }
+    removeItem(key) {
+      localStorage.removeItem(this.prefix + key);
+    }
+  };
+  var local_storage_default = new PathLocalStorage(params_default.baseURL);
+
+  // ns-hugo:D:\GitHub\Blog\themes\hugo-theme-bootstrap\assets\js\mode\index.ts
+  var MODE_AUTO = "auto";
+  var MODE_DARK = "dark";
+  var MODE_LIGHT = "light";
+  var modes = [MODE_AUTO, MODE_DARK, MODE_LIGHT];
+  var ModeToggle = class {
+    constructor() {
+      // Cache key.
+      this.key = "mode";
+      let mode = local_storage_default.getItem(this.key);
+      if (!mode) {
+        mode = params_default.color;
+      }
+      this.mode = modes.includes(mode) ? mode : MODE_AUTO;
+    }
+    run() {
+      this.setMode(this.mode);
+      window.addEventListener("load", () => {
+        this.initListeners();
+        this.active(this.mode);
+      });
+    }
+    initListeners() {
+      this.items = document.querySelectorAll(".mode-item");
+      this.items.forEach((ele) => {
+        ele.addEventListener("click", () => {
+          const mode = ele.getAttribute("data-color-mode");
+          this.setMode(mode);
+          this.active(mode);
+        });
+      });
+      window.matchMedia("(prefers-color-scheme: dark)").addListener((e) => {
+        if (this.isAuto()) {
+          this.setMode(e.matches ? "dark" : "light");
+        }
+      });
+    }
+    isAuto() {
+      return this.mode === MODE_AUTO;
+    }
+    // Active the relative HTML elements.
+    active(mode) {
+      var _a, _b;
+      this.mode = mode;
+      local_storage_default.setItem(this.key, mode);
+      this.items.forEach((ele) => {
+        const classList = ele.querySelector(".dropdown-item").classList;
+        if (ele.getAttribute("data-color-mode") === mode) {
+          classList.add("active");
+        } else {
+          classList.remove("active");
+        }
+      });
+      const icon = (_a = document.querySelector('.mode-item[data-color-mode="' + mode + '"] .mode-icon')) == null ? void 0 : _a.cloneNode(true);
+      if (!icon) {
+        return;
+      }
+      icon.setAttribute("id", "modeIcon");
+      (_b = document.querySelector("#modeIcon")) == null ? void 0 : _b.replaceWith(icon);
+    }
+    setMode(value) {
+      if (value === "auto") {
+        value = ModeToggle.getPreferredMode();
+      }
+      console.debug(`Switch to ${value} mode`);
+      document.documentElement.setAttribute("data-bs-theme", value);
+      const event = new CustomEvent("hbs:mode", { detail: { mode: value } });
+      document.dispatchEvent(event);
+    }
+    static getPreferredMode() {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+      }
+      return "light";
+    }
+  };
+  var mode_default = ModeToggle;
+
+  // ns-hugo:D:\GitHub\Blog\themes\hugo-theme-bootstrap\assets\js\palettes\index.ts
+  var PaletteSelector = class {
+    constructor() {
+      this.key = "palette";
+    }
+    run() {
+      const palette = this.getPalette();
+      if (palette) {
+        this.setPalette(palette);
+      }
+      window.addEventListener("load", () => {
+        this.initPalette();
+      });
+    }
+    initPalette() {
+      const selected = this.getPalette();
+      document.querySelectorAll(".palette").forEach((element) => {
+        const paletteId = element.getAttribute("id").replace("palette-", "");
+        if (paletteId === selected) {
+          element.classList.add("active");
+        }
+        element.addEventListener("click", () => {
+          this.setPalette(paletteId);
+          document.querySelector(".palette.active").classList.remove("active");
+          element.classList.add("active");
+        });
+      });
+    }
+    getPalette() {
+      const palette = local_storage_default.getItem(this.key);
+      if (palette) {
+        return palette;
+      }
+      const paletteMeta = document.documentElement.getAttribute("data-palette");
+      if (paletteMeta) {
+        return paletteMeta;
+      }
+      return "";
+    }
+    setPalette(palette) {
+      console.debug(`switch to palette: ${palette}`);
+      document.documentElement.setAttribute("data-palette", palette);
+      local_storage_default.setItem(this.key, palette);
+    }
+  };
+  var palettes_default = PaletteSelector;
+
+  // <stdin>
+  new mode_default().run();
+  new palettes_default().run();
+})();
